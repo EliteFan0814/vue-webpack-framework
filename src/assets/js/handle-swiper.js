ee301ab5cb74757ac79801a681c02d0660fdab6f
EliteFan <fan552426811@outlook.com>
2021-12-13 00:44:54 +0800

    fpc add

---
+++ src/assets/js/handle-swiper.js
@@ -0,0 +1,3 @@
+// import Swiper from './swiper-bundle.min.js'
+
+var swiper = new Swiper(".mySwiper", {});
--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import 'reset-css'
-// import './assets/css/base.scss'
 import './assets/css/index.scss'
-// import html from "../public/index.html";
+import './assets/js/swiper-bundle.min.css'
+// import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
+import Swiper from './assets/js/swiper-bundle.min.js'
+var mySwiper = new Swiper('.swiper', {
+  direction: 'horizontal', // 垂直切换选项
+  loop: true, // 循环模式选项
+  autoplay: false, //可选选项，自动滑动
+  // 如果需要分页器
+  pagination: {
+    el: '.swiper-pagination'
+  },
+
+  // 如果需要前进后退按钮
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+
+  // 如果需要滚动条
+  // scrollbar: {
+  //   el: '.swiper-scrollbar'
+  // }
+})
 var html = require('../public/index.html')
-// const $ = require('jquery')(window)
-console.log($('.body-wrap'))
